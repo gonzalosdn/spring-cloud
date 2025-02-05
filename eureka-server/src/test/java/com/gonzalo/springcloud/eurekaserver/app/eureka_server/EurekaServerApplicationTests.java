ce8d242d126b21c27fc9932987431b325402c59a
Gonzalo Silva <gonzalo200696@gmail.com>
2025-02-05 18:39:24 -0300

    agregando eureka-server

---
+++ eureka-server/src/test/java/com/gonzalo/springcloud/eurekaserver/app/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzalo.springcloud.eurekaserver.app.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
